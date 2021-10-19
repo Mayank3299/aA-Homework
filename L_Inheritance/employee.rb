2c02c6df34ff65f530f176510d8c26e490791d9a
mayank <mayank.agarwal0903@gmail.com>
2021-10-19 13:16:27 +0530

    Learned Inheritance

---
+++ L_Inheritance/employee.rb
@@ -0,0 +1,50 @@
+class Employee
+    attr_reader :name, :title, :salary
+    attr_accessor :boss
+
+    def initialize(name, title, salary, boss=nil)
+        @name= name
+        @title= title
+        @salary= salary
+        self.boss= boss
+    end
+
+    def boss=(boss)
+        @boss= boss
+        boss.add_employee(self) unless boss.nil?
+    end
+
+    def bonus(multiplier)
+        bonus = (salary) * multiplier
+    end
+end
+
+class Manager < Employee
+
+    attr_reader :employees
+
+    def initialize(name,title,salary, boss=nil)
+        super(name, title, salary, boss)
+        @employees=[]
+    end
+    
+    def bonus(multiplier)
+        self.total_subsalary * multiplier
+    end
+
+    def add_employee(subordinate)
+        @employees << subordinate
+    end
+
+    def total_subsalary
+        total_subsalary=0
+        @employees.each do |employee|
+            if employee.is_a?(Manager)
+                total_subsalary+= employee.salary + employee.total_subsalary
+            else
+                total_subsalary+= employee.salary
+            end
+        end
+        total_subsalary
+    end
+end
