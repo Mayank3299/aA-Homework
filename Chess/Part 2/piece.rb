8dfd4f3a43dfcc562fcc5b3c5d44105d6bc4d7c0
mayank <mayank.agarwal0903@gmail.com>
2021-10-28 15:00:51 +0530

    Implemented valid moves method to check for check

--- Chess/Part 2/piece.rb
+++ Chess/Part 2/piece.rb
@@ -3,13 +3,17 @@ class Piece
     attr_reader :pos, :board, :color
 
     def initialize(board, color, pos)
+        raise "Invalid pos" unless board.valid_pos?(pos)
+        raise "Invalid color" unless %i(white black).include?(color)
         @board= board
         @color= color
         @pos= pos
+
+        @board.add_piece(self, pos)
     end
 
     def valid_moves
-        moves
+        moves.reject{|end_pos| move_into_check?(end_pos)}
     end 
 
     def empty?
@@ -19,4 +23,10 @@ class Piece
     def to_s
         " #{symbol} "
     end 
+
+    def move_into_check?(end_pos)
+        test= board.dup
+        test.move_piece(pos, end_pos)
+        test.in_check?(color)
+    end
 end
