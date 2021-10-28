25683527d3293c7d533124471468e7ca688c9208
mayank <mayank.agarwal0903@gmail.com>
2021-10-28 14:59:44 +0530

    Implemented check and checkmat funcions

--- Chess/Part 2/board.rb
+++ Chess/Part 2/board.rb
@@ -17,13 +17,20 @@ class Board
         @grid[row][col]= piece
     end
 
-    def move_piece(start_pos, end_pos)
-        if self[start_pos].nil?
-            raise "Starting position is not valid"
+    def move_piece(turn_color, start_pos, end_pos)
+        if self[start_pos].empty?
+            raise "No piece at starting position"
         end
+
         piece= self[start_pos]
+        if piece.color != turn_color
+            raise "Please move piece of your own color"
+        elsif !piece.moves.include?(end_pos)
+            raise "Please choose a valid ending position"
+        end
         self[end_pos]= piece
-        self[start_pos]= nil
+        self[start_pos]= @sentinel
+        piece.pos= end_pos
     end
 
     def valid_pos?(pos)
@@ -34,19 +41,42 @@ class Board
         self[pos].empty?
     end
 
-    #def render
-    #    @grid.each_with_index do |row, i|
-    #        row.each_with_index do |piece, j|
-    #            @grid[i][j]= Queen.new(self, :black, [i,j])
-    #        end
-    #    end
-    #    
-    #    @grid.each do |rows|
-    #        rows.each do |piece|
-    #            print piece.to_s.colorize(:background => :light_yellow)
-    #        end
-    #        puts ""
-    #    end
-    #    nil
-    #end
+    def in_check?(color)
+        king_pos= find_king(color).pos
+        pieces.any?{|p| p.color != color && p.moves.include?(king_pos)}
+    end
+
+    def checkmate?(color)
+        return false unless in_check?(color)
+
+        pieces.select{|p| p.color == color}.all? do |piece|
+            piece.valid_moves.empty?
+        end
+    end
+
+    def find_king(color)
+        king_pos= pieces.find{|p| p.color == color && p.is_a?(King)}
+        king_pos || (raise "King not found")
+    end
+
+    def pieces
+        @grid.flatten.reject{&:empty?}
+    end
+
+    def dup
+        new_board= Board.new
+
+        pieces.each do |p|
+            p.class.new(new_board, p.color, p.pos)
+        end
+        
+        new_board
+    end
+
+    def add_piece(piece, pos)
+        raise "Position not empty" unless empty?(pos)
+
+        self[pos] = piece
+    end
+
 end
