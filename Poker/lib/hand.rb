4b9ab8a205aa6bf5250f270d1b36c4da01f68797
mayank <mayank.agarwal0903@gmail.com>
2021-12-07 04:49:25 +0530

    Basic functionality of hand class

--- Poker/lib/hand.rb
+++ Poker/lib/hand.rb
@@ -1,7 +1,32 @@
+require_relative 'card'
+
 class Hand
 
     attr_reader :cards
     def initialize(cards)
-        @cards= cards
+        raise 'Please provide 5 cards' if cards.count != 5
+        @cards= cards.sort
+    end
+
+    def trade_cards(old_cards, new_cards)
+        raise 'must have 5 cards' unless old_cards.count == new_cards.count
+        raise 'cannot discard unowned card' unless owned?(old_cards)
+        take_cards(new_cards) && discard_cards(old_cards) && sort!
+    end
+    private
+    def take_cards(cards)
+        @cards += cards
+    end
+
+    def discard_cards(old_cards)
+        old_cards.each {|card| cards.delete(card)}
+    end
+
+    def sort!
+        @cards.sort!
+    end
+
+    def owned?(old_cards)
+        old_cards.all? {|card| @cards.include?(card)}
     end
 end
