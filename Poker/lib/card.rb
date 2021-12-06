e4a289880dd262fa14fe5209b4880a36dcf1734d
mayank <mayank.agarwal0903@gmail.com>
2021-12-06 18:51:34 +0530

    Added comparable module to card class and spaceship method for comparison

--- Poker/lib/card.rb
+++ Poker/lib/card.rb
@@ -1,26 +1,30 @@
 class Card
-    
-    POSSIBLE_VALUES= [
-            :ace, 
-            :king, 
-            :queen, 
-            :jack, 
-            :ten, 
-            :nine, 
-            :eight, 
-            :seven,
-            :six,
-            :five,
-            :four,
-            :three,
-            :two,
-        ]
-        POSSIBLE_TYPES= [
-            :hearts,
-            :spades,
-            :clubs,
-            :diamonds
-        ]
+    include Comparable
+    POSSIBLE_VALUES= 
+    {
+        :ace => 'A', 
+        :king => 'K', 
+        :queen => 'Q', 
+        :jack => 'J', 
+        :ten => 10, 
+        :nine => 9, 
+        :eight => 8, 
+        :seven => 7,
+        :six => 6,
+        :five => 5,
+        :four => 4,
+        :three => 3,
+        :two => 2
+    }
+
+    POSSIBLE_TYPES= 
+    {
+        :hearts => '♥',
+        :spades => '♠',
+        :clubs => '♣',
+        :diamonds => '♦' 
+    }
+
     attr_reader :value, :type
 
     def initialize(value, type)
@@ -28,5 +32,27 @@ class Card
         raise "Please provide a valid suit" unless POSSIBLE_TYPES.include?(type)
         @value= value
         @type= type
+    end
+
+    def self.values
+        POSSIBLE_VALUES.keys
+    end
+
+    def self.types
+        POSSIBLE_TYPES.keys
+    end
+
+    def ==(other_obj)
+        (self.type == other_obj.type) && (self.value == other_obj.value)
+    end
+   
+    def <=> (obj)
+        if self == obj
+            0
+        elsif value != obj.value
+            Card.values.index(value) <=> Card.values.index(obj.value)
+        elsif type != obj.type
+            Card.types.index(type) <=> Card.types.index(obj.type)
+        end
     end
 end
