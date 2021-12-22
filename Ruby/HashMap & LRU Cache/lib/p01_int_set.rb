b5b4799d314b58e28324837ef052862d76805315
mayank <mayank.agarwal0903@gmail.com>
2021-12-22 22:16:08 +0530

    Resizing set according to input

--- Ruby/HashMap & LRU Cache/lib/p01_int_set.rb
+++ Ruby/HashMap & LRU Cache/lib/p01_int_set.rb
@@ -1,44 +1,65 @@
 class MaxIntSet
+
+  attr_reader :store
   def initialize(max)
+    @max= max
+    @store= Array.new(max + 1, false)
   end
 
   def insert(num)
+    validate!(num)
+    @store[num]= true
   end
 
   def remove(num)
+    validate!(num)
+    return nil unless include?(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num.between?(0, self.store.length - 1)
   end
 
   def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
   end
 end
 
 
 class IntSet
+
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
+    num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
@@ -47,26 +68,35 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
-
+  
   def insert(num)
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if num_buckets < @count
   end
 
   def remove(num)
+    return false unless include?(num)
+    self[num].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
@@ -74,5 +104,9 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store= store
+    @count= 0
+    @store = Array.new(num_buckets * 2) {Array.new}
+    old_store.flatten.each {|ele| insert(ele)}
   end
 end
