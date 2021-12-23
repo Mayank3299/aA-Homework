2f8f34afff4286ecae6079de25f08002da71fd86
mayank <mayank.agarwal0903@gmail.com>
2021-12-23 17:32:13 +0530

    Created a doubly linked list with sentinel nodes

--- Ruby/HashMap & LRU Cache/lib/p04_linked_list.rb
+++ Ruby/HashMap & LRU Cache/lib/p04_linked_list.rb
@@ -16,11 +16,23 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    self.prev.next= self.next if self.prev
+    self.next.prev= self.prev if self.next
+    self.prev= nil
+    self.next= nil
+    self
   end
 end
 
 class LinkedList
+  include Enumerable
+  
+  attr_reader :head, :tail
   def initialize
+    @head= Node.new
+    @tail= Node.new
+    @head.next= @tail
+    @tail.prev= @head
   end
 
   def [](i)
@@ -29,30 +41,68 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : @head.next
   end
 
   def last
+    empty? ? nil : @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each do |node|
+      if node.key == key
+        return node.val
+      end
+    end
+
+    nil
   end
 
   def include?(key)
+    each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+    node= Node.new(key, val)
+    @tail.prev.next= node
+    node.prev= @tail.prev
+    node.next= @tail
+    @tail.prev= node
+
+    node
   end
 
   def update(key, val)
+    each do |node|
+      if node.key == key
+        node.val = val
+        return node
+      end
+    end
   end
 
   def remove(key)
+    each do |node|
+      if node.key == key
+        node.remove
+        return node.val
+      end
+    end
   end
 
   def each
+    current_node= @head.next 
+    while current_node != @tail
+      yield current_node
+      current_node= current_node.next
+    end 
   end
 
   # uncomment when you have `each` working and `Enumerable` included
