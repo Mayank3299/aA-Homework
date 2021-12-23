0e8a6dd0383d589608d267ec9976be8b735991ff
mayank <mayank.agarwal0903@gmail.com>
2021-12-23 15:29:31 +0530

    Added hashing to our previous resizing set

--- Ruby/HashMap & LRU Cache/lib/p03_hash_set.rb
+++ Ruby/HashMap & LRU Cache/lib/p03_hash_set.rb
@@ -1,5 +1,5 @@
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -7,18 +7,27 @@ class HashSet
   end
 
   def insert(key)
+    return false if include?(key)
+    self[key.hash] << key
+    @count +=1
+    resize! if @count > num_buckets
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    return false unless include?(key)
+    self[key.hash].delete(key)
+    @count -=1
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +35,9 @@ class HashSet
   end
 
   def resize!
+    old_store= self.store
+    @count= 0
+    @store= Array.new(num_buckets * 2) {Array.new}
+    old_store.flatten.each {|ele| insert(ele)}
   end
 end
