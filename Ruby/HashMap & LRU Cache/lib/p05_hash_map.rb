d59eedcbeebf348fb1906131ac65aae8d34ae5a7
mayank <mayank.agarwal0903@gmail.com>
2021-12-24 16:59:57 +0530

    Implemented a HashMap with using LinkedList as buckets

--- Ruby/HashMap & LRU Cache/lib/p05_hash_map.rb
+++ Ruby/HashMap & LRU Cache/lib/p05_hash_map.rb
@@ -1,6 +1,7 @@
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_accessor :count
 
   def initialize(num_buckets = 8)
@@ -9,18 +10,35 @@ class HashMap
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
+    resize! if self.count >= num_buckets
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
   end
 
   def get(key)
+    bucket(key).get(key)
   end
 
   def delete(key)
+    removal= bucket(key).remove(key) 
+    @count -= 1 if removal
+    removal
   end
 
   def each
+    @store.each do |bucket|
+      bucket.each do |link|
+        yield [link.key, link.val]
+      end
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -41,9 +59,19 @@ class HashMap
   end
 
   def resize!
+    old_store= @store
+    @count= 0
+    @store= Array.new(num_buckets * 2) {LinkedList.new}
+
+    old_store.each do |bucket|
+      bucket.each {|link| set(link.key, link.val)}
+    end
   end
 
   def bucket(key)
     # optional but useful; return the bucket corresponding to `key`
+    hashed_value= key.hash
+    bucket= hashed_value % num_buckets
+    @store[bucket]
   end
 end
