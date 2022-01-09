82f6a2954932c5bf51fd21850f537a2bc426576f
mayank <mayank.agarwal0903@gmail.com>
2022-01-09 22:53:36 +0530

    Select basics 4th bonus exercise

--- SQL/SQL Zoo/lib/04a_countries_bonus.rb
+++ SQL/SQL Zoo/lib/04a_countries_bonus.rb
@@ -14,6 +14,19 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      gdp > (
+        SELECT
+          MAX(gdp)
+        FROM
+          countries
+        WHERE
+          continent = 'Europe'
+      );
   SQL
 end
 
@@ -21,6 +34,21 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT
+      continent,
+      name,
+      area
+    FROM
+      countries
+    WHERE
+      area = (
+        SELECT
+          MAX(area)
+        FROM
+          countries AS c2
+        WHERE
+          countries.continent = c2.continent
+      );
   SQL
 end
 
@@ -28,5 +56,20 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT
+      name,
+      continent
+    FROM
+      countries
+    WHERE
+      population > 3 * (
+        SELECT
+          MAX(population)
+        FROM
+          countries c2
+        WHERE
+          c2.name != countries.name
+          AND c2.continent = countries.continent
+      );
   SQL
 end
